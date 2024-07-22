44132f11bb917fa9e270a0413cb67cbc0fc65c12
BAKPE François Amour Yélian <yelian.bakpe@epitech.eu>
2024-07-22 01:23:52 +0100

    ajout methode get

--- src/stores/postIt.js
+++ src/stores/postIt.js
@@ -13,7 +13,7 @@ export const usePostsIt = defineStore('postsItStore', {
 
         async getAllPostIt() {
             const apiUrl = "https://post-it.epi-bluelock.bj/notes/"
-            await fetch(apiUrl)
+            await fetch(apiUrl, {method:"GET"})
                 .then((response) => response.json())
                 .then((response) => { (this.postsIt = response.notes) })
 
